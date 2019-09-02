f1c85cdfe06cb2434cd43131714e98cad3e3bf1c
Doug Swain <pseudoramble@gmail.com>
2019-09-02 14:19:05 -0400

    Parse height/weight before dispatching update.

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -17,24 +17,27 @@ type Model = { description: BodyDescription option }
 // The Msg type defines what events/actions can occur while the application is running
 // the state of the application changes *only* in reaction to these events
 type Msg =
-  | UpdateHeight of string
-  | UpdateWeight of string
+  | UpdateHeight of int
+  | UpdateWeight of int
 
 // defines the initial state and initial command (= side-effect) of the application
 let init () : Model * Cmd<Msg> =
   let initialModel = { description = Some { height = 0; weight = 0 } }
   (initialModel, Cmd.none)
 
-// The update function computes the next state of the application based on the current state and the incoming events/messages
+// The update function computes the next state of the application based on the
+//  current state and the incoming events/messages
 // It can also run side-effects (encoded as commands) like calling the server via Http.
 // these commands in turn, can dispatch messages to which the update function will react.
 let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
   match currentModel.description, msg with
     | Some desc, UpdateHeight h ->
-      let nextModel = { currentModel with description = Some { height = int32 h; weight = desc.weight } }
+      let nextModel = {
+        currentModel with description = Some { height = h; weight = desc.weight } }
       (nextModel, Cmd.none)
     | Some desc, UpdateWeight w ->
-      let nextModel = { currentModel with description = Some { height = desc.height; weight = int32 w } }
+      let nextModel = {
+        currentModel with description = Some { height = desc.height; weight = w } }
       (nextModel, Cmd.none)
     | _ ->
       (currentModel, Cmd.none)
@@ -53,8 +56,8 @@ let view (model : Model) (dispatch : Msg -> unit) =
       ]
       BmiForm.form {
         onCalculate = (fun _ -> printfn "lol")
-        onHeightChange = (fun e -> dispatch (UpdateHeight !!e.target?value))
-        onWeightChange = (fun e -> dispatch (UpdateWeight !!e.target?value))
+        onHeightChange = (fun e -> dispatch (UpdateHeight (int32 !!e.target?value)))
+        onWeightChange = (fun e -> dispatch (UpdateWeight (int32 !!e.target?value)))
       }
     ]
 
