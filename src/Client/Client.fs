3c6b0fc1a6eb355f473e25e2635b5eec561cf484
Doug Swain <pseudoramble@gmail.com>
2019-09-02 13:27:41 -0400

    Remove old code from client-side.

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -48,54 +48,20 @@ let update (msg : Msg) (currentModel : Model) : Model * Cmd<Msg> =
     | _, InitialCountLoaded initialCount->
       let nextModel = { Counter = Some initialCount }
       (nextModel, Cmd.none)
-    | _ -> 
+    | _ ->
       (currentModel, Cmd.none)
 
-
-let safeComponents =
-  let components =
-    span [ ] [
-      a [ Href "https://github.com/SAFE-Stack/SAFE-template" ] [
-        str "SAFE  "
-        str Version.template
-      ]
-      str ", "
-      a [ Href "https://saturnframework.github.io" ] [ str "Saturn" ]
-      str ", "
-      a [ Href "http://fable.io" ] [ str "Fable" ]
-      str ", "
-      a [ Href "https://elmish.github.io" ] [ str "Elmish" ]
-      str ", "
-      a [ Href "https://fulma.github.io/Fulma" ] [ str "Fulma" ]
-    ]
-
-  span [ ] [
-    str "Version "
-    strong [ ] [ str Version.app ]
-    str " powered by: "
-    components
-  ]
 
 let show = function
   | { Counter = Some counter } -> string counter.Value
   | { Counter = None   } -> "Loading..."
 
-let button txt onClick =
-  Button.button [ Button.IsFullWidth; Button.Color IsPrimary; Button.OnClick onClick ] [
-    str txt
-  ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
   div [] [
     Navbar.navbar [ Navbar.Color IsPrimary ] [
-      Navbar.Item.div [ Navbar.Item.IsHoverable; Navbar.Item.HasDropdown ] [
-        Navbar.Link.a [ ] [ str "Welcome" ]
-        Navbar.Dropdown.div [] [
-          Navbar.Item.a [] [ str "Home" ]
-        ]
-      ]
       Navbar.Item.div [ ] [
-        Heading.h2 [ ] [ str "SAFE Template" ]
+        Heading.h2 [ ] [ str "BMI Calculator" ]
       ]
     ]
 
@@ -105,23 +71,13 @@ let view (model : Model) (dispatch : Msg -> unit) =
       ]
       BmiForm.form {
         onChange = (fun e -> printfn "%A" (!!e.target?value));
-        onCalculate = (fun _ -> printfn "lol") 
+        onCalculate = (fun _ -> printfn "lol")
       }
-    ]
-
-    Container.container [ ] [
-      Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [
-        Heading.h3 [] [ str ("Press buttons to manipulate counter: " + show model) ]
-      ]
-      Columns.columns [] [
-        Column.column [] [ button "-" (fun _ -> dispatch Decrement) ]
-        Column.column [] [ button "+" (fun _ -> dispatch Increment) ]
-      ]
     ]
 
     Footer.footer [ ] [
       Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ] [
-        safeComponents
+        str "lol thx <3"
       ]
     ]
   ]
